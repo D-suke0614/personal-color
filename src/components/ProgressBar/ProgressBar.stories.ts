b672420756d9c3df826389d14ea3d8f349de6bf1
Hinako <>
2025-02-08 17:00:37 +0900

    プログレスバーのコンポーネントを作成

---
+++ src/components/ProgressBar/ProgressBar.stories.ts
@@ -0,0 +1,21 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import ProgressBar from '@/components/ProgressBar/ProgressBar'; // importする
+
+const meta = {
+  title: 'Components/ProgressBar', // storybook上のパス
+  component: ProgressBar, // コンポーネント名入れる
+  tags: ['autodocs'],
+  parameters: {
+    layout: 'centered',
+  },
+} as Meta<typeof ProgressBar>; // コンポーネント名入れる
+
+export default meta;
+type Story = StoryObj<typeof meta>; //おまじない
+
+export const Primary: Story = {
+  args: {
+    initialProgress: 0,
+  },
+};
---
+++ src/components/ProgressBar/ProgressBar.tsx
@@ -0,0 +1,34 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+
+type ProgressBarProps = {
+  initialProgress: progressPercentage;
+};
+
+// 仮で10ずつの数値を設定
+type progressPercentage = 0 | 10 | 20 | 30 | 40 | 50 | 60 | 70 | 80 | 90 | 100;
+
+const ProgressBar: React.FC<ProgressBarProps> = ({ initialProgress }) => {
+  const [progress, setProgress] = useState<progressPercentage>(initialProgress);
+
+  useEffect(() => {
+    setProgress(initialProgress);
+  }, [initialProgress]);
+
+  return (
+    <div className="relative my-4 h-full">
+      <div className="h-11 overflow-hidden rounded-full bg-[#b4b3b3] shadow-[3px_3px_3px_#c0c0c0]">
+        <div
+          className="h-full w-fit rounded-full bg-gradient-to-r from-[#E3FDFB] via-[#FFCEDE] to-[#DCBCF6] transition-all duration-1000 ease-out"
+          style={{ width: `${progress}%` }}
+        />
+      </div>
+      <div className="absolute top-0 h-11 w-full text-center font-bold leading-[2.75rem]">
+        {progress}%
+      </div>
+    </div>
+  );
+};
+
+export default ProgressBar;
