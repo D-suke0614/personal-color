abb7f517151e5153490fa31adf02367ad5b4abec
Daisuke <d.suke.kida@gmail.com>
2025-02-24 17:18:15 +0900

    feat: トーストコンポーネントの実装

---
+++ src/components/Toast/Toast.tsx
@@ -0,0 +1,23 @@
+'use client';
+
+import clsx from 'clsx';
+
+type Props = {
+  message: string;
+  isShow: boolean;
+};
+
+const Toast = ({ message, isShow }: Props) => {
+  return (
+    <div
+      className={clsx(
+        'rounded-xl bg-[#28d186] px-28 py-3 text-2xl text-white opacity-80',
+        `${isShow ? 'block' : 'hidden'}`,
+      )}
+    >
+      {message}
+    </div>
+  );
+};
+
+export default Toast;
