49260e1aa380dcd074ff6f5ccdc108eff899f52e
Daisuke <d.suke.kida@gmail.com>
2025-02-26 00:16:22 +0900

    feat: 背景色の修正

--- src/components/Toast/Toast.tsx
+++ src/components/Toast/Toast.tsx
@@ -11,7 +11,7 @@ const Toast = ({ message, isShow }: Props) => {
   return (
     <div
       className={clsx(
-        'rounded-xl bg-[#28d186] px-28 py-3 text-2xl text-white opacity-80',
+        'rounded-xl bg-black px-28 py-3 text-2xl text-white opacity-80',
         `${isShow ? 'block' : 'hidden'}`,
       )}
     >
